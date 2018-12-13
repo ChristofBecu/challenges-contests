1d8ee82ab8e25e6ac81685777cd16a74c974e33d
bedawang <christof.becu@gmail.com>
2018-12-13 16:31:27 +0100

    c++ solution added

---
+++ codechef/pratice/beginner/addTwoNumbers/addTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, t = 0;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
